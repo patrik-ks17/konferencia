ea2b46d55f8c9f55f589686d7e61bd5289b8c89d
KasaPatrik <kasaip02@gmail.com>
2022-12-15 09:49:53 +0100

    asd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import Presentations from './Presentations'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header>IOK VKK Demo</Header>
+      <Main>
+        <h1>Előadások</h1>
+        <Presentations />
+      </Main>
+      <Footer>
+        &copy; HTTP Alapítvány
+      </Footer>
+    </>
   );
 }
 
 export default App;
+
+
+const Header = styled.header`
+  height: 58px;
+  background-color: rgb(20,71,92);
+  color: white;
+  font-size: 18px;
+  padding: 15px;
+`
+
+const Footer = styled.footer`
+  height: 58px;
+  background-color: rgb(20,71,92);
+  color: white;
+  font-size: 18px;
+  padding: 15px;
+`
+
+const Main = styled.main`
+  padding: 15px;
+  background-color: rgb(214,244,245);
+  height: calc(100vh - 58px - 58px);
+  overflow: auto;
+`
---
+++ src/Presentations.js
@@ -0,0 +1,94 @@
+import styled from 'styled-components'
+
+const Presentations = () => {
+	return (
+		<>
+			{presentations.map((presentation, index) => {
+				<Presentation presentation={presentation} key={index}/>
+			})}
+		</>
+	)
+}
+
+const Presentation = (props) => { 
+	const {presentation} = props
+	return (
+		<PresentationWrapper>
+			<Time>{presentation.startTime.substring(11, 16)}</Time>
+			<Title>{presentation.title}</Title>
+			<Presenter>
+				{presentations.presenter.name} {presentation.presenter.company}, {presentation.presenter.title},
+				<div>
+					<img src={presentation.presenter.photo.url} alt={presentation.presenter.name}/>
+				</div>
+			</Presenter>
+		</PresentationWrapper>
+	)
+}
+
+const Presenter = styled.div`
+	font-size: 14px;
+	font-weight: 400px;
+	color: rgb(150,150,150);
+	img {
+		width: 80px;
+		border-radius: 50%;
+		padding-top: 10px;
+	}
+`
+
+const PresentationWrapper = styled.div`
+	padding: 15px;
+`
+
+const Time = styled.div`
+	font-size: 14px;
+	font-weight: 400;
+	color: rgb(150,150,150)
+`
+
+const Title = styled.div`
+	font-size: 18px;
+	font-weight: 600;
+`
+
+export default Presentations
+
+const presentations = [
+	{
+		 title: "Köszöntő",
+		 startTime: "2022-05-25T09:00:00+01:00",
+		 presenter: {
+			  title: "ügyvezető igazgató",
+			  photo: {
+					url: "https://www.datocms-assets.com/70643/1651850041-1644788738-dalos_otto.png"
+			  },
+			  name: 'Dalos Ottó',
+			  company: 'Cisco Magyarország'
+		 }
+	},
+	{
+		 title: "Befektetés az IT oktatásba",
+		 startTime: "2022-05-25T09:20:00+01:00",
+		 presenter: {
+			  title: "vezérigazgató",
+			  photo: {
+					url: "https://www.datocms-assets.com/70643/1651850083-1647423382-gonda_gabor_2.png"
+			  },
+			  name: 'Gonda Gábor',
+			  company: 'T Systems Magyarország Zrt'
+		 }
+	},
+	{
+		 title: "A digitális evolúció és az oktatás",
+		 startTime: "2022-05-25T09:45:00+01:00",
+		 presenter: {
+			  title: "angyalbafektető, ügyvezető",
+			  photo: {
+					url: "https://www.datocms-assets.com/70643/1651850126-1646229711-balogh_peter_2.png"
+			  },
+			  name: 'Balogh Péter',
+			  company: 'Baconsult Kft'
+		 }
+	}
+]
